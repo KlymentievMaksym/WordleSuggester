196d8b278affbdf3b108fae5bef52d651eeba5d5
Dadmon <113892002+KlymentievMaksym@users.noreply.github.com>
2026-06-25 17:13:11 +0300

    refactor: speed it all up

--- src/wordle_suggester/cogs/wordle.py
+++ src/wordle_suggester/cogs/wordle.py
@@ -4,25 +4,49 @@ import discord
 import discord.ext.commands as cmnd
 import pandas as pd
 
-from wordle_suggester.utils.entropy import calculate_best_word
+from wordle_suggester.utils.entropy import calculate_best_word, create_colors_from_str, color_word_using_colors
 
 class Wordle(cmnd.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
+        self.available_words = pd.read_csv(Path(__file__).parent.parent / "data" / "filtered-wordle-words.csv", header=None)
+        self.available_words_list = self.available_words[0].apply(lambda x: x.lower()).tolist()
 
     @discord.slash_command(name="suggest", description="Suggest next possible n words")
     async def suggest(
         self,
         ctx: discord.ApplicationContext,
         word: str = discord.Option(str, description="Enter word to start from"), # type: ignore
-        colors: str = discord.Option(str, description="Enter colors in format like '00201', 'ddgdy', '  g y' or 'gray, gray, green, gray, yellow"), # type: ignore
+        colors: str = discord.Option(str, description="Enter colors in format like '00201', 'ddgdy', '  g y' or 'gray, gray, green, gray, yellow'"), # type: ignore
         best_n: int = discord.Option(int, description="Enter integer to receive that or smaller amount of answers", default=1), # type: ignore
         private: bool = discord.Option(bool, description="Enter True or False to choose whether share message with others or not share", default=True) # type: ignore
     ):
-        available_words = pd.read_csv(Path(__file__).parent.parent / "data" / "filtered-wordle-words.csv", header=None)
-        available_words = available_words[0].apply(lambda x: x.lower()).tolist()
-        best_words = calculate_best_word(word.lower(), colors.lower(), available_words, best_n)
-        await ctx.respond(f"Best **{min(best_n, len(best_words))}** recommended words: **{' | '.join([word.upper() for word in best_words]) if best_words else 'There is no recommended word available'}**", ephemeral=private)
+        try:
+            word = word.lower()
+            best_words, best_entropies = calculate_best_word(word, colors, self.available_words_list, best_n)
+            # text_to_respond = f"Best **{min(best_n, len(best_words))}** recommended words: **{' | '.join([word.upper() for word in best_words]) if best_words else 'There is no recommended word available'}**"
+
+            colors_tuple = create_colors_from_str(colors)
+            color_base_word = color_word_using_colors(word.upper(), colors_tuple)
+
+            text_to_respond = f"```ansi\n{color_base_word}```Best **{min(best_n, len(best_words))}** recommended next words:\n**```ansi\n{'{results}' if best_words else 'There is no recommended word available'}```**"
+
+            colors_best_words = [[0 if color != 2 else 2 for color in colors_tuple] for word in best_words]
+            yellow_letters = []
+            for letter, color in zip(word, colors_tuple):
+                if color == 1:
+                    yellow_letters.append(letter)
+            for word_index, recommended_word in enumerate(best_words):
+                for letter_index, letter in enumerate(recommended_word):
+                    if letter in yellow_letters and colors_best_words[word_index][letter_index] != 2:
+                        colors_best_words[word_index][letter_index] = 1
+
+            results = f"{'\n'.join([f"{color_word_using_colors(recommended_word.upper(), tuple(colors_list))} | {entropy:.3f}" for recommended_word, colors_list, entropy in zip(best_words, colors_best_words, best_entropies)])}"
+            text_to_respond = text_to_respond.format(results=results)
+
+            await ctx.respond(text_to_respond, ephemeral=private)
+        except ValueError as e:
+            await ctx.respond(e, ephemeral=True)
 
 
 def setup(bot: discord.Bot):
--- src/wordle_suggester/utils/entropy.py
+++ src/wordle_suggester/utils/entropy.py
@@ -1,74 +1,98 @@
 import numpy as np
 from collections import defaultdict, Counter
 
-alternatives = {"g": "green", "2": "green", "y": "yellow", "1": "yellow", "d": "gray", " ": "gray", "0": "gray"}
-accessable = ["green", "yellow", "gray"]
+alternatives = {"g": 2, "green": 2, "2": 2, "yellow": 1, "y": 1, "1": 1, "gray": 0, "d": 0, " ": 0, "0": 0}
+accessable = [0, 1, 2]
 
-def filter_words(word: str, colors: list[str], available_words: list[str]) -> list[str]:
+def filter_words(word: str, colors: tuple[int, ...], available_words: list[str]) -> list[str]:
+    # print(colors, word)
+
     suitable_words = []
     for candidate in available_words:
-        is_valid = True
+        candidate_pattern = get_pattern(word, candidate)
 
-        for i in range(5):
-            guess_letter = word[i]
-            candidate_letter = candidate[i]
-            color = colors[i].lower()
-            if color in alternatives:
-                color = alternatives[color]
-            if color not in accessable:
-                raise ValueError(f"Not suitable color for '{color}'")
-
-            if ((color == "green" and guess_letter != candidate_letter)
-                or (color == "gray" and guess_letter in candidate)
-                or (color == "yellow" and (guess_letter == candidate_letter or guess_letter not in candidate))
-            ):
-                is_valid = False
-                break
-
-        if is_valid:
+        if candidate_pattern == colors:
             suitable_words.append(candidate)
+
+    # print(suitable_words)
     return suitable_words
 
 
-def get_pattern(guess: str, secret: str) -> list[str]:
-    pattern = ["gray" for _ in range(5)]
-    available_letters = list(secret)
+def get_pattern(guess: str, secret: str) -> tuple[int, ...]:
+    pattern = [0] * 5
+    letter_counts = Counter(secret)
 
     for i in range(5):
         if guess[i] == secret[i]:
-            pattern[i] = "green"
-            available_letters.remove(secret[i])
+            pattern[i] = 2
+            letter_counts[guess[i]] -= 1
 
     for i in range(5):
-        if guess[i] in available_letters and pattern[i] != "green":
-            pattern[i] = "yellow"
-            available_letters.remove(guess[i])
+        if guess[i] in letter_counts and letter_counts[guess[i]] > 0 and pattern[i] != 2:
+            pattern[i] = 1
+            letter_counts[guess[i]] -= 1
 
-    return pattern
+    return tuple(pattern)
+
+# def get_pattern_multiple(guesses: list[str], secrets: list[str]) -> :
 
 def calculate_entropy(guess: str, available_words: list[str]) -> float:
     pattern_counts = defaultdict(int)
     total_count = len(available_words)
 
     for secret in available_words:
-        pattern_list = get_pattern(guess, secret)
-        pattern_str = ",".join(pattern_list)
-        pattern_counts[pattern_str] += 1
+        patterns = get_pattern(guess, secret)
+        # pattern_str = ",".join(pattern_list)
+        pattern_counts[patterns] += 1
         
     entropy = 0.0
     
-    for pattern, count in pattern_counts.items():
+    for count in pattern_counts.values():
         Pr = count/total_count
         entropy -= Pr * np.log2(Pr)
     
     return entropy
 
-def calculate_best_word(word: str, colors: str, available_words: list[str], best_n: int = 1) -> list[str]:
+def create_colors_from_str(colors: str) -> tuple[int, ...]:
     if len(colors.split(",")) == 1:
-        colors_list = list(colors)
+        colors_list = list(color.lower() for color in colors)
     else:
-        colors_list = list(color.strip() for color in colors.split(","))
-    # print(colors_list)
+        colors_list = list(color.strip().lower() for color in colors.split(","))
+
+    if len(colors_list) != 5:
+        raise ValueError(f"Expected colors length 5, received {len(colors_list)} ({colors_list})")
+
+    colors_result = [0] * 5
+    for color_index in range(len(colors_list)):
+        color = colors_list[color_index]
+
+        if color in alternatives:
+            color = alternatives[color]
+
+        if color not in accessable:
+            raise ValueError(f"Not suitable color for '{color}'")
+
+        colors_result[color_index] = color
+    return tuple(colors_result)
+
+def color_word_using_colors(word: str, colors: tuple[int, ...]):
+    formater = {
+        2: "\033[2;32m",
+        1: "\033[2;33m",
+        0: "\033[2;30m",
+        "reset": "\033[0m"
+    }
+    color_word = formater["reset"]
+    for color_index in range(len(colors)):
+        color_word += formater[colors[color_index]] + word[color_index] + formater["reset"]
+    return color_word
+
+def calculate_best_word(word: str, colors: str, available_words: list[str], best_n: int = 1) -> tuple[list[str], list[float]]:
+    if len(word) != 5:
+        raise ValueError(f"Expected word length 5, received {len(word)}")
+
+    colors_list = create_colors_from_str(colors)
+
     available_words = filter_words(word, colors_list, available_words)
 
     guesses_entropy = defaultdict(float)
@@ -78,4 +102,5 @@ def calculate_best_word(word: str, colors: str, available_words: list[str], best
 
     sorted_items = sorted(guesses_entropy.items(), key=lambda item: item[1], reverse=True)
     sorted_best_guesses = [item[0] for item in sorted_items]
-    return sorted_best_guesses[:best_n]
+    sorted_best_entropies = [item[1] for item in sorted_items]
+    return sorted_best_guesses[:best_n], sorted_best_entropies[:best_n]
